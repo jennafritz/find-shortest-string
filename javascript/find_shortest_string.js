b11981a7a997981e7ede5d05ad432e9a319da5c6
jennafritz <jennafritz24@gmail.com>
2021-08-15 17:09:52 -0400

    Completed

--- javascript/find_shortest_string.js
+++ javascript/find_shortest_string.js
@@ -1,5 +1,13 @@
 function findShortestString(arr) {
   // type your code here
+
+  let arrayOfLengths = arr.map(string => string.length)
+
+  let shortest = Math.min(...arrayOfLengths)
+
+  let shortestIndex = arrayOfLengths.indexOf(shortest)
+
+  return arr[shortestIndex]
 }
 
 if (require.main === module) {
@@ -7,12 +15,14 @@ if (require.main === module) {
   console.log("Expecting: 'a'");
   console.log("=>", findShortestString(['aaa', 'a', 'bb', 'ccc']));
 
-  console.log("");
+  console.log("Expecting: 'this'");
+  console.log("=>", findShortestString(['hopefully', 'this', 'works', 'properly']));
 
   console.log("Expecting: 'hi'");
   console.log("=>", findShortestString(['cat', 'hi', 'dog', 'an']));
 
-  console.log("");
+  console.log("Expecting: 'my'");
+  console.log("=>", findShortestString(['my', 'fingers', 'are', 'crossed']));
 
   console.log("Expecting: 'lily'");
   console.log("=>", findShortestString(['flower', 'juniper', 'lily', 'dadelion']));
@@ -23,4 +33,11 @@ if (require.main === module) {
 module.exports = findShortestString;
 
 // Please add your pseudocode to this file
+  // map through array of strings, returning length of string
+  // find minimum element in array of lengths and get index
+  // return element in string array at that index
+
 // And a written explanation of your solution
+  // we create a new array that stores the lengths of all the strings, in order
+  // we find the smallest number in that array of lengths and grab its index
+  // then return the string at that index in the original array
--- ruby/find_shortest_string.rb
+++ ruby/find_shortest_string.rb
@@ -1,17 +1,30 @@
 def find_shortest_string(arr)
   # type your code in here
+
+  array_of_lengths = arr.map do |string|
+    string.length
+  end
+
+  shortest = array_of_lengths.min
+
+  shortest_index = array_of_lengths.index(shortest)
+
+  arr[shortest_index]
+
 end
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'a'"
   puts "=>", find_shortest_string(['aaa', 'a', 'bb', 'ccc'])
 
-  puts
+  puts "Expecting: 'this'"
+  puts "=>", find_shortest_string(['hopefully', 'this', 'works', 'properly'])
 
   puts "Expecting: 'hi'"
   puts "=>", find_shortest_string(['cat', 'hi', 'dog', 'an'])
 
-  puts
+  puts "Expecting: 'my'"
+  puts "=>", find_shortest_string(['my', 'fingers', 'are', 'crossed'])
 
   puts "Expecting: 'lily'"
   puts "=>", find_shortest_string(['flower', 'juniper', 'lily', 'dadelion'])
